98cf2f724e8cedd715be6a7ba225116b2e7499a3
FitiaFranschesca <fitiafranschesca@gmail.com>
2026-04-16 11:53:58 +0400

    ci: ajout fichier de overage pour sonar

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/test/setup.js'
+    setupFiles: './src/test/setup.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['lcov', 'text'],
+      reportsDirectory: './coverage',
+    }
   }
 })
